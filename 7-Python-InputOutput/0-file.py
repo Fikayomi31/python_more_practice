405b93a46fb574dd591c1d0199b70c2ace8bd683
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-10 22:21:54 +0100

    0-file

--- 7-Python-InputOutput/0-file.py
+++ 7-Python-InputOutput/0-file.py
@@ -1,9 +1,20 @@
 import os
 
 with open("tests/mydata.txt", "w", encoding="utf-8") as myfile:
-    myfile.write("Some rsndon text\nMore random file\nAnd some more")
+    myfile.write("Some randon text\nMore random file\nAnd some more")
 
 with open("tests/mydata.txt", encoding="utf-8") as myfile:
     print(myfile.read())
 
-print(myfile.close)
+
+    # seek used to omit a given character
+    print(myfile.seek(1))
+    print()
+    print(myfile.seek(2))
+    print()
+    print(myfile.seek(3))
+    print()
+    print(myfile.read())
+
+
+print(myfile.close())
