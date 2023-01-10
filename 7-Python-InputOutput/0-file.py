d97014abff5b97443731ef1abbda50ec95a94db5
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-10 22:14:17 +0100

    text

--- 7-Python-InputOutput/0-file.py
+++ 7-Python-InputOutput/0-file.py
@@ -1,9 +1,9 @@
 import os
 
-with open("mydata.txt", mode="w", encoding="utf-8") as myfile:
+with open("tests/mydata.txt", "w", encoding="utf-8") as myfile:
     myfile.write("Some rsndon text\nMore random file\nAnd some more")
 
-with open("mydata.txt", encoding="utf-8") as myfile:
+with open("tests/mydata.txt", encoding="utf-8") as myfile:
     print(myfile.read())
 
 print(myfile.close)
