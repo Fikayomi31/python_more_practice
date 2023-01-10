291ddbb4f99d82ab43f61fb8b181c9b7f3ea319f
Fikayomi31 <fikayoogundijo@gmail.com>
2023-01-10 22:00:16 +0100

    file

--- 7-Python-InputOutput/1-read.py
+++ 7-Python-InputOutput/1-read.py
@@ -3,3 +3,8 @@ with open('mydata.txt', encoding="utf-8") as f:
     for line in read_data:
 
         print(line, end='')
+        
+
+with open('tests/my_file_0.txt', encoding="utf-8") as e_file:
+    read_file = e_file.read()
+    print(read_file)
